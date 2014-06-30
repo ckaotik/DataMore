b044069a4b00aed2f5ac688b07c0dce66a774b62
ckaotik <ckaotik@gmail.com>
2014-06-30 12:08:36 +0200

    Further development on mail module

--- modules/mails.lua
+++ modules/mails.lua
@@ -1,15 +1,16 @@
 if true then return end
 
-local addonName, ns, _ = ...
+local addonName, addon, _ = ...
+local moduleName = 'DataMore_Mails'
+local mails      = addon:NewModule('mails', 'AceEvent-3.0') -- 'AceConsole-3.0'
 
-local addonName  = "DataMore_Mails"
-local addon      = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "AceEvent-3.0")
-   _G[addonName] = addon
-
-local characters    = DataStore:GetCharacters()
+local characters    = DataStore:GetCharacters() -- TODO: heirlooms may be mailed x-realm!
 local thisCharacter = DataStore:GetCharacter()
 local realm = GetRealmName()
       realm = realm:gsub(' ', '')
+
+local DEFAULT_STATIONERY = 'Interface\\Icons\\INV_Misc_Note_01'
+local DEFAULT_ICON = 'Interface\\Icons\\INV_Crate_02'
 
 -- these subtables need unique identifier
 local AddonDB_Defaults = {
@@ -17,13 +18,80 @@ local AddonDB_Defaults = {
 		Characters = {
 			['*'] = {				-- ["Account.Realm.Name"]
 				lastUpdate = nil,
-				LFGs = {},
+				Mails = {
+					['*'] = {
+						sender = nil,
+						subject = nil,
+						message = nil,
+						stationery = DEFAULT_STATIONERY,
+						expires = 0, -- x < 0: deleted in x, x > 0: returned in x
+						lastUpdate = nil,
+
+						money = 0, -- x < 0: COD, x > 0: earned
+						attachments = {
+							['*'] = {
+								itemID = nil,
+								itemLink = nil, -- only used if item is enchanted etc
+								count = 0,
+							},
+						},
+					}
+				},
 			}
 		}
 	}
 }
 
-local function SendMail_Hook(recipient, subject, body, ...)
+local function ScanMail(mails, index)
+	local mail = {}
+
+	local icon, stationery, sender, subject, money, CODAmount, daysLeft, numAttachments, wasRead, wasReturned, textCreated, canReply = GetInboxHeaderInfo(index)
+
+	-- local senderName, senderRealm = strsplit('-', sender)
+	mail.sender = sender
+	mail.subject = subject
+	mail.stationery = stationery ~= DEFAULT_STATIONERY and stationery or nil
+	mail.money = money > 0 and money or -1*CODAmount
+	mail.expires = time() + daysLeft*24*60*60
+	mail.lastUpdate = time()
+
+	--[[
+	-- this marks mail as read
+	local bodyText, texture, isTakeable, isInvoice = GetInboxText(index)
+	mail.message = bodyText
+	if isInvoice then
+		local invoiceType, itemName, playerName, bid, buyout, deposit, consignment, moneyDelay, etaHour, etaMin = GetInboxInvoiceInfo(index)
+	end
+	--]]
+
+	mail.attachments = {}
+	for attachmentIndex = 1, _G.ATTACHMENTS_MAX_RECEIVE do
+		local itemlink = GetInboxItemLink(index, attachmentIndex)
+		local _, _, count = GetInboxItem(index, attachmentIndex)
+		if itemLink then
+			local itemID = mails.GetLinkID(itemLink)
+			local _, simpleLink = GetItemInfo(itemID)
+			table.insert(mail.attachments, {
+				itemID = itemID,
+				itemLink = itemLink ~= simpleLink and itemLink or nil,
+				count = count,
+			})
+		end
+	end
+
+	table.insert(mails.ThisCharacter.Mails, mail)
+end
+
+local function ScanInbox()
+	local character = mails.ThisCharacter
+	wipe(character.Mails)
+
+	for index = 1, GetInboxNumItems() do
+		ScanMail(index)
+	end
+end
+
+local function OnSendMail(recipient, subject, body)
 	recipient = recipient:lower()
 	for characterName, characterKey in pairs(characters) do
 		characterName = characterName:lower()
@@ -32,7 +100,7 @@ local function SendMail_Hook(recipient, subject, body, ...)
 		end
 
 		if strlower(characterName) == strlower(recipient) then						-- if recipient is a known alt ..
-			local character = addon.db.global.Characters[characterKey]
+			local character = mails.db.global.Characters[characterKey]
 
 			if mailAttachments then
 				for k, v in pairs(mailAttachments) do		--  .. save attachments into his mailbox
@@ -40,7 +108,7 @@ local function SendMail_Hook(recipient, subject, body, ...)
 						icon = v.icon,
 						link = v.link,
 						count = v.count,
-						sender = UnitName("player"),
+						sender = UnitName('player'),
 						lastCheck = time(),
 						daysLeft = 30,
 					} )
@@ -61,7 +129,7 @@ local function SendMail_Hook(recipient, subject, body, ...)
 					icon = mailIcon,
 					text = body,
 					subject = subject,
-					sender = UnitName("player"),
+					sender = UnitName('player'),
 					lastCheck = time(),
 					daysLeft = 30,
 				} )
@@ -85,19 +153,19 @@ local PublicMethods = {
 	-- GetCurrencyCaps = _GetCurrencyCaps,
 }
 
-function addon:OnInitialize()
-	addon.db = LibStub("AceDB-3.0"):New(addonName .. "DB", AddonDB_Defaults)
+function mails:OnInitialize()
+	self.db = LibStub('AceDB-3.0'):New(moduleName .. 'DB', AddonDB_Defaults)
 
-	DataStore:RegisterModule(addonName, addon, PublicMethods)
+	DataStore:RegisterModule(moduleName, self, PublicMethods)
 	-- DataStore:SetCharacterBasedMethod("GetCurrencyCaps")
 end
 
-function addon:OnEnable()
-	hooksecurefunc('SendMail', SendMail_Hook)
+function mails:OnEnable()
+	hooksecurefunc('SendMail', OnSendMail)
 
-	-- addon:RegisterEvent("LFG_LOCK_INFO_RECEIVED", UpdateLFGStatus)
+	-- self:RegisterEvent("LFG_LOCK_INFO_RECEIVED", UpdateLFGStatus)
 end
 
-function addon:OnDisable()
-	-- addon:UnregisterEvent("LFG_LOCK_INFO_RECEIVED")
+function mails:OnDisable()
+	-- self:UnregisterEvent("LFG_LOCK_INFO_RECEIVED")
 end
